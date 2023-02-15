cec566b7a345a1451abe7b5e823ca22464d87d17
Michael Harris <mike.n.harris1@gmail.com>
2023-02-15 11:04:30 -0500

    success

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
 
-
+document.addEventListener("DOMContentLoaded", (e) => {
 //request data from server to get all ramen objects
 fetch("http://localhost:3000/ramens")
 .then(r => r.json())
-.then(data => {
-    console.log(data)
-   data.forEach(data => console.log(data))
-   renderRamen(data)
+.then(ramenArray => {
+    console.log(ramenArray)
+    ramenArray.forEach(element => {
+        console.log(element)  
+    const image = document.createElement("img")
+    image.src = element.image
+    document.getElementById("ramen-menu").appendChild(image)
+    
+ populateSpace (image, element)
 })
-
-
-function renderRamen(data) {
-const container = document.querySelector("#ramen-detail")
-const image = document.querySelector("#ramen-detail > img")
-const h2 = document.querySelector("#ramen-detail > h2")
-const h3 = document.querySelector("#ramen-detail > h3")
-
+})
+}) 
+function populateSpace(image, element) {
+image.addEventListener("click", (e) => {
+console.log(element)
+let foo = document.querySelector(".detail-image")
+foo.src = element.image
+let name = document.querySelector(".name")
+name.textContent = element.name
+let restaurant = document.querySelector(".restaurant")
+restaurant.textContent = element.restaurant
+})
 }
 
 
-//add img tag to "ramen-menu" div
-const image = document.createElement("img");
-const element = document.getElementById("ramen-menu")
-element.appendChild(image)
 
-const myImage = document.getElementById("ramen-detail")
-element.appendChild(image).style.display = image
+// See all ramen images in the div with the id of ramen-menu.
+ //display the image for each of the ramen using an img tag inside the #ramen-menu div.
 
 
 
 
 
-// const myImage = new Image(100, 200);
-// myImage.src = 'picture.jpg';
-// document.body.appendChild(myImage);
+
+
+//    Click on an image from the #ramen-menu div and see all the info about that ramen displayed inside the #ramen-detail div and where it says insert comment here and insert rating here.
